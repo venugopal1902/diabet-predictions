87d9d7727bbb01601bcdbbe8f9f7970368ad2cad
venugopal1902 <76394287+venugopal1902@users.noreply.github.com>
2022-02-23 15:55:56 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -45,12 +45,5 @@ if __name__ == '__main__':
 # if __name__ == '__main__':
 #        start_server(predict, port=8000, debug=True)
 
-app.add_url_rule('/', 'webio_view', webio_view(predict),
-            methods=['GET', 'POST', 'OPTIONS'])
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    args = parser.parse_args()
 
-    start_server(predict, port=args.port)
 
