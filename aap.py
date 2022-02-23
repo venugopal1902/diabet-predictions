eff4e2fab054e74a63d8588ab5a21b557d98871f
venugopal1902 <76394287+venugopal1902@users.noreply.github.com>
2022-02-23 15:51:55 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -1,11 +1,10 @@
-from pywebio.platform.flask import webio_view
-from pywebio import STATIC_PATH
-from flask import Flask, send_from_directory
+from select import select
+from flask import Flask
 from pywebio.input import *
 from pywebio.output import *
-import argparse
+from pywebio.output import put_text 
 from pywebio import start_server
-
+import pandas as pd
 import pickle
 import numpy as np
 model = pickle.load(open('venu.pkl', 'rb'))
@@ -13,25 +12,38 @@ app = Flask(__name__)
 
 
 def predict():
-    Age = input('Enter your Age',type=NUMBER)
-    BMI = input('Enter your Bobymass Index',type=FLOAT)
-    Glucose =  input('Enter your Glucoselevel',type=NUMBER)
-    BloodPressure = input('Enter your Blood Pressure',type=NUMBER)
-    Pregnancies = input('Enter no.of Pregnancies(if your are a man enter 0)',type=NUMBER)
-    SkinThickness = input('Enter your Skin Thickness',type=NUMBER)
-    DiabetesPedigreeFunction = input('Enter your DiabetesPedigreeFunction',type=FLOAT)
-    Insulin = input('Enter your Insulin',type=NUMBER)                                 
     
+    Age = input('Enter your Age',type=NUMBER,name=f's{2}')
+    BMI = input('Enter your Bobymass Index',type=FLOAT,name=f's{3}')
+    Glucose =  input('Enter your Glucoselevel',type=NUMBER,name=f's{4}')
+    BloodPressure = input('Enter your Blood Pressure',type=NUMBER,name=f's{5}')
+    Pregnancies = input('Enter no.of Pregnancies(if your are a man enter 0)',type=NUMBER,name=f's{6}')
+    SkinThickness = input('Enter your Skin Thickness',type=NUMBER,name=f's{8}')
+    DiabetesPedigreeFunction = input('Enter your DiabetesPedigreeFunction',type=FLOAT,name=f's{9}')
+    Insulin = input('Enter your Insulin',type=NUMBER,name=f's{10}')    
+                                 
+    d = [Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]
+    d = input_group('basic info',d)
+    d= list(d.values())
 
-    output = model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+    output = model.predict([d])
     
 
     if output == 1 :
-        put_text("Diabetes : Positive")
+        put_html("<h2>Diabetes : Positive</h2>")
     
     else: 
-        put_text('Diabetes : Negative')
-        
+        put_html('<h2>Diabetes : Negative</h2>')
+    put_html('<a href="/" style="background-color:blue;margin-left:350px;color:white;;padding :8px;border-radius:5px;font-size:30px;text-decoration:none;box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">Home</a>') 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
+
+    start_server(predict, port=args.port)
+# if __name__ == '__main__':
+#        start_server(predict, port=8000, debug=True)
 
 app.add_url_rule('/', 'webio_view', webio_view(predict),
             methods=['GET', 'POST', 'OPTIONS'])
